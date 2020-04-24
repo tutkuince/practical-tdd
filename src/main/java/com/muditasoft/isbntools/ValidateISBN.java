b1955d62f12a13ba843b926bf18a5150f5af6159
Tutku Ince <tutku.ince@outlook.com>
2020-04-24 16:46:59 +0300

    ISBN test

--- src/main/java/com/muditasoft/isbntools/ValidateISBN.java
+++ src/main/java/com/muditasoft/isbntools/ValidateISBN.java
@@ -1,4 +1,14 @@
 package com.muditasoft.isbntools;
 
 public class ValidateISBN {
+
+    public boolean checkISBN(String isbnNumber) {
+        int total = 0;
+        int tenCheckDigits = 10;
+
+        for (int i = 0; i < tenCheckDigits; i++) {
+            total += isbnNumber.charAt(i) * (tenCheckDigits - i);
+        }
+        return total % 11 == 0;
+    }
 }
--- src/test/java/com/muditasoft/isbntools/ValidateISBNTest.java
+++ src/test/java/com/muditasoft/isbntools/ValidateISBNTest.java
@@ -1,7 +1,25 @@
 package com.muditasoft.isbntools;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidateISBNTest {
+
+    @Test
+    void checkAValidISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue(result, "First value");
+        result = validator.checkISBN("0140177396");
+        assertTrue(result, "Second value");
+    }
+
+    @Test
+    void checkAnInvalidISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
 
 }
