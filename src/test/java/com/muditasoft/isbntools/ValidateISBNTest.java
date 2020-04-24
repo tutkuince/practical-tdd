23256c612a36d53cdc48e94b802493c5a3b9f6c3
Tutku Ince <tutku.ince@outlook.com>
2020-04-24 16:50:49 +0300

    Refactoring process for Test class

--- src/test/java/com/muditasoft/isbntools/ValidateISBNTest.java
+++ src/test/java/com/muditasoft/isbntools/ValidateISBNTest.java
@@ -1,23 +1,29 @@
 package com.muditasoft.isbntools;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidateISBNTest {
 
+    private ValidateISBN validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new ValidateISBN();
+    }
+
     @Test
     void checkAValidISBN() {
-        ValidateISBN validator = new ValidateISBN();
-        boolean result = validator.checkISBN("0140449116");
-        assertTrue(result, "First value");
-        result = validator.checkISBN("0140177396");
-        assertTrue(result, "Second value");
+        assertAll("Validate ISBN",
+                () -> assertTrue(validator.checkISBN("0140449116"), "First Value"),
+                () -> assertTrue(validator.checkISBN("0140177396"), "Second Value")
+                );
     }
 
     @Test
     void checkAnInvalidISBN() {
-        ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449117");
         assertFalse(result);
     }
