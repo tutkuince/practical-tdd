28409e10d7b575be609f3d1035acb7d671a24068
Tutku Ince <tutku.ince@outlook.com>
2020-05-04 22:39:49 +0300

    Refactoring with Character.getNumericValue

--- src/main/java/com/muditasoft/isbntools/ValidateISBN.java
+++ src/main/java/com/muditasoft/isbntools/ValidateISBN.java
@@ -11,10 +11,17 @@ public class ValidateISBN {
 
         for (int i = 0; i < tenCheckDigits; i++) {
 
+            // If last digit is X, X is 10.
+            if (i  == tenCheckDigits - 1 && isbnNumber.charAt(tenCheckDigits - 1) == 'X') {
+                total += 10 * (tenCheckDigits - i);
+                break;
+            }
+
+
             if (!Character.isDigit(isbnNumber.charAt(i)))
                 throw new NumberFormatException("ISBN numbers can only contain numeric digits.");
 
-            total += isbnNumber.charAt(i) * (tenCheckDigits - i);
+            total += Character.getNumericValue(isbnNumber.charAt(i)) * (tenCheckDigits - i);
         }
         return total % 11 == 0;
     }
