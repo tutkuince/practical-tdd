f0c3529e75b78f72202d95bbcab78ca7d52d19cc
Tutku Ince <tutku.ince@outlook.com>
2020-05-04 22:05:19 +0300

    Character is digit check with if control flow

--- src/main/java/com/muditasoft/isbntools/ValidateISBN.java
+++ src/main/java/com/muditasoft/isbntools/ValidateISBN.java
@@ -10,6 +10,10 @@ public class ValidateISBN {
             throw new NumberFormatException();
 
         for (int i = 0; i < tenCheckDigits; i++) {
+
+            if (!Character.isDigit(isbnNumber.charAt(i)))
+                throw new NumberFormatException("ISBN numbers can only contain numeric digits.");
+
             total += isbnNumber.charAt(i) * (tenCheckDigits - i);
         }
         return total % 11 == 0;
